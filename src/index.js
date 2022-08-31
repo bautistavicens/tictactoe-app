5b194ad37fafaa79f7922188487dd620476206f0
Bautista <bautivicens@gmail.com>
2022-08-30 23:08:27 -0300

    release 1.3

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import TicTacToe from './components/TicTacGame.jsx';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <TicTacToe />
+    <Footer />
   </React.StrictMode>
 );
 
